d78cda5a73b1649fa30fa0ac115ac6682e1f9edb
Gerb-24 <gerbenlamers@gmail.com>
2022-04-05 11:19:25 +0200

    orientations added
    the prototype is also in the right orientation now

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src.PyVMF import *
+import numpy as np
+
 prototypeVMF = load_vmf("umon_ws_prototype.vmf")
 toBeDetailedVMF = load_vmf("umon_ws_test.vmf")
 detailedVMF = new_vmf()
-wallshotTexture = "CUSTOMDEV/DEV_MEASUREWALL01BLU"
+texture = "CUSTOMDEV/DEV_MEASUREWALL01BLU"
 
 def createDuplicateVMF(vmf):
     duplicateVMF = new_vmf()
@@ -35,7 +37,48 @@ def getDimensionsOfSolid( solid ):
     zMax = max([ vertex.z for vertex in allVertices ])
     return xMin, xMax, yMin, yMax, zMin, zMax
 
+def getOrientationOfSolid( solid ):
+    texturedSide = solid.get_texture_sides(texture)[0]
+    texturedSideVertices = texturedSide.get_vertices()
+    xMin, xMax, yMin, yMax, _, _ = getDimensionsOfSolid( solid )
+    if all( vertex.y == yMin for vertex in texturedSideVertices ):
+        return "-y"
+    if all( vertex.x == xMax for vertex in texturedSideVertices ):
+        return "x"
+    if all( vertex.y == yMax for vertex in texturedSideVertices ):
+        return "y"
+    if all( vertex.x == xMin for vertex in texturedSideVertices ):
+        return "-x"
+
+def rotateSolidAroundZAxis(solid: Solid, deg):
+    solid.rotate_z(Vertex( 0, 0, 0), deg )
+    rad = deg/360*2*np.pi
+    rotMat = np.array([[np.cos(rad), -np.sin(rad), 0], [np.sin(rad), np.cos(rad), 0], [0, 0, 1]])
+    for side in solid.get_sides():
+        uaxisArray = np.array([float(side.uaxis.x), float(side.uaxis.y), float(side.uaxis.z)])
+        uaxisArray = rotMat.dot(uaxisArray)
+        side.uaxis.x, side.uaxis.y, side.uaxis.z = str(uaxisArray[0]), str(uaxisArray[1]), str(uaxisArray[2])
+
+        vaxisArray = np.array([float(side.vaxis.x), float(side.vaxis.y), float(side.vaxis.z)])
+        vaxisArray = rotMat.dot(vaxisArray)
+        side.vaxis.x, side.vaxis.y, side.vaxis.z = str(vaxisArray[0]), str(vaxisArray[1]), str(vaxisArray[2])
+
+
 def movePrototypeToSolid( solid, prototypeVMF ):
+
+    prototypeDuplicate = createDuplicateVMF(prototypeVMF)
+    orientationOfSolid = getOrientationOfSolid( solid )
+
+    orientationToRotationDict = {
+        "-y": 0,
+        "x": 90,
+        "y": 180,
+        "-x":270,
+    }
+
+    for solidToRotate in prototypeDuplicate.get_solids():
+        rotateSolidAroundZAxis( solidToRotate, orientationToRotationDict[orientationOfSolid] )
+
     topVertexManipulationBox = VertexManipulationBox( -512, 512, -512, 512, 0, 512)
     bottomVertexManipulationBox = VertexManipulationBox( -512, 512, -512, 512, -512, 0)
     backVertexManipulationBox = VertexManipulationBox( -512, 512, 0, 512, -512, 512)
@@ -43,7 +86,6 @@ def movePrototypeToSolid( solid, prototypeVMF ):
     startVertexManipulationBox = VertexManipulationBox( 0, 512, -512, 512, -512, 512)
     endVertexManipulationBox = VertexManipulationBox( -512, 0, -512, 512, -512, 512)
 
-    prototypeDuplicate = createDuplicateVMF(prototypeVMF)
     topVertices = topVertexManipulationBox.getVerticesInBox(prototypeDuplicate)
     bottomVertices = bottomVertexManipulationBox.getVerticesInBox(prototypeDuplicate)
     backVertices = backVertexManipulationBox.getVerticesInBox(prototypeDuplicate)
@@ -69,6 +111,7 @@ def movePrototypeToSolid( solid, prototypeVMF ):
         vertex.move(384, 0, 0)
 
     xMin, xMax, yMin, yMax, zMin, zMax = getDimensionsOfSolid( solid )
+
     for vertex in topVertices:
         vertex.move(0, 0, 1024)
     for vertex in backVertices:
